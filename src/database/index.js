ee5e8382f19f8779bb766cf762cecc4745b214da
kodcanlisi <developerindiee@gmail.com>
2021-04-09 11:33:54 +0300

    Connect to database

---
+++ src/database/index.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const connectMongodb = (databaseName) => {
+    mongoose.connect(process.env.MONGO_DB + databaseName, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    });
+};
+
+const DB = (databaseName) => {
+    connectMongodb(databaseName);
+    return mongoose.connection;
+};
+
+export default DB;
