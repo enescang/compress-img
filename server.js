d7a0d1a0f7f14bef8c6e07c24019682d2a6bcf70
kodcanlisi <developerindiee@gmail.com>
2021-04-09 12:01:41 +0300

    File upload example

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import DB from './src/database';
+import multer from 'multer';
 
 const app = express();
+
+const upload = multer({ dest: 'temp/' })
 
 const { PORT } = process.env;
 
 app.listen(PORT,()=>{
     console.log('server')
+})
+
+app.post('/data', upload.single('img'), (req, res)=>{
+    if(!req.file)
+        return res.send("dosya yok");
+    
+    res.json(req.file)
 })
 
 const db = DB(process.env.DB_NAME);
