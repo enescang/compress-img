133ebe9b01efbbdcbf6935115f5ecae6260e03ad
kodcanlisi <developerindiee@gmail.com>
2021-04-13 14:02:10 +0300

    image path test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import DB from './src/database';
 import multer from 'multer';
-
+import path from 'path';
 const app = express();
 
 const upload = multer({ dest: 'temp/' })
@@ -15,8 +15,8 @@ app.listen(PORT,()=>{
 app.post('/data', upload.single('img'), (req, res)=>{
     if(!req.file)
         return res.send("dosya yok");
-    
-    res.json(req.file)
+    const full_path = path.resolve("./temp", req.file.filename);
+    res.json(full_path);
 })
 
 const db = DB(process.env.DB_NAME);
