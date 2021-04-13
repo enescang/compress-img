1de22219defbff1214a115f52706505229b0cba3
kodcanlisi <developerindiee@gmail.com>
2021-04-13 14:53:45 +0300

    First compression test

--- server.js
+++ server.js
@@ -2,21 +2,46 @@ import express from 'express';
 import DB from './src/database';
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 const app = express();
 
 const upload = multer({ dest: 'temp/' })
-
+const {compress} = require("compress-images/promise");
 const { PORT } = process.env;
 
 app.listen(PORT,()=>{
     console.log('server')
-})
+});
 
-app.post('/data', upload.single('img'), (req, res)=>{
+
+const MyFun=async(name)=> {
+  let INPUT_path_to_your_images = `temp/${name}`;
+  let OUTPUT_path = "build/";
+  
+  const img = await compress({
+      source: INPUT_path_to_your_images,
+      destination: OUTPUT_path,
+      enginesSetup: {
+       // jpg: { engine: "mozjpeg", command: ["-quality", "60"] },
+        png: { engine: "pngcrush", command:['-reduce', '-brute']  },
+        // svg: { engine: "svgo", command: "--multipass"  },
+        //gif: { engine: "gif2webp", command: ["--colors", "64", "--use-col=web"] }
+      }
+  });
+  const {err, statistic, completed} = img
+  return img;
+  }
+
+app.post('/data', upload.single('img'), async(req, res)=>{
     if(!req.file)
         return res.send("dosya yok");
     const full_path = path.resolve("./temp", req.file.filename);
-    res.json(full_path);
+    let extArray = req.file.mimetype.split("/");
+    let extension = extArray[extArray.length - 1];
+    fs.renameSync(full_path, full_path+"."+extension);
+    
+    const result = await MyFun(req.file.filename+"."+extension);
+    res.json(result);
 })
 
 const db = DB(process.env.DB_NAME);
