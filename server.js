aba18f9df19481c1bb6e64e100161765e00b1a99
kodcanlisi <developerindiee@gmail.com>
2021-05-02 01:44:19 +0300

    Add /gif request

--- server.js
+++ server.js
@@ -3,48 +3,90 @@ import DB from './src/database';
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
+import cors from 'cors';
+
 const app = express();
 
+app.use(cors())
 const upload = multer({ dest: 'temp/' })
-const {compress} = require("compress-images/promise");
+const { compress } = require("compress-images/promise");
 const { PORT } = process.env;
 
-app.listen(PORT,()=>{
-    console.log('server')
+app.listen(PORT, () => {
+  console.log('server')
 });
 
 
-const MyFun=async(name)=> {
-  let INPUT_path_to_your_images = `temp/${name}`;
-  let OUTPUT_path = "build/";
-  
-  const img = await compress({
+const MyFun = async ({ fileName = null, engineSetup = {} }) => {
+  try{
+    let INPUT_path_to_your_images = `temp/${fileName}`;
+    let OUTPUT_path = "build/";
+    console.log(engineSetup)
+    const img = await compress({
       source: INPUT_path_to_your_images,
       destination: OUTPUT_path,
-      enginesSetup: {
+      enginesSetup:
         //jpg: { engine: "mozjpeg", command: ["-quality", "70"] },
         //png: { engine: "pngcrush", command:[]  },
         //svg: { engine: "svgo", command: false },
-        gif: {engine: 'gifsicle', command: ['--scale', ' 0.1']}
-      }
-  });
-  const {err, statistic, completed} = img
-  return img;
+        //gif: {engine: 'gifsicle', command: ['--scale', ' 0.1']}
+        engineSetup
+  
+    });
+    const { err, statistic, completed } = img
+    return img;
+  } catch(e){
+    throw new Error(e)
   }
+}
 
-app.post('/data', upload.single('img'), async(req, res)=>{
-    if(!req.file)
-        return res.send("dosya yok");
+app.post('/data', upload.single('img'), async (req, res) => {
+  if (!req.file)
+    return res.send("dosya yok");
+  const full_path = path.resolve("./temp", req.file.filename);
+  let extArray = req.file.mimetype.split("/");
+  let extension = extArray[extArray.length - 1];
+  if (extension.match(/xml/)) {
+    extension = "svg"
+  }
+  fs.renameSync(full_path, full_path + "." + extension);
+
+  const result = await MyFun(req.file.filename + "." + extension);
+  res.json(result);
+})
+
+app.post('/gif', upload.single('img'), async (req, res) => {
+  console.log("GIF_REQUEST COMING", req.body);
+  try {
+    if (!req.file)
+      return res.status(401).send({error:'Dosya Seçilmedi'});
     const full_path = path.resolve("./temp", req.file.filename);
     let extArray = req.file.mimetype.split("/");
     let extension = extArray[extArray.length - 1];
-    if(extension.match(/xml/)){
-      extension = "svg"
+    if (extension.toString() !== 'gif') {
+      return res.send(401).send({ error: 'IMAGE_NOT_GIF' });
     }
+
+    //Rename image
     fs.renameSync(full_path, full_path+"."+extension);
-    
-    const result = await MyFun(req.file.filename+"."+extension);
+
+    const GIF_ENGINE = { gif: { engine: 'gifsicle', command: ['--scale', ' 1'] } };
+    const options = { fileName: req.file.filename + "." + extension, engineSetup: GIF_ENGINE };
+    //Check scale is defined
+    if(req.body.scale){
+      if (Number(req.body.scale) <=1 && Number(req.body.scale) >=0) {
+        GIF_ENGINE.gif.command[1] = Number(req.body.scale);
+      }else{
+        return res.status(401).send({error:'scale değeri 0-1 arasında bir değer olmalıdır'})
+      }
+    }
+
+    const result = await MyFun(options);
     res.json(result);
+  } catch (e) {
+    res.send(e.message)
+  }
+
 })
 
 const db = DB(process.env.DB_NAME);
